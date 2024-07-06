e2aaf251530ddd88d0f3a55990f97d5834353460
121986-code <167398427+121986-code@users.noreply.github.com>
2024-07-06 09:23:39 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perwaani Record Management System</title>
+    <script src=""></script>
+    <link rel="stylesheet" href="../CSS/menu.css">
+</head>
+<body>
+    
+    <div class="wrapper">
+        <div class="sidebar">
+            <h2>Perwaani system</h2>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="items.php">Add New Item</a></li>
+                <li><a href="cash.php">Add Cash Receipt</a></li>
+                <li><a href="users.php">View Items</a></li>
+                <li><a href="../Views/items_views.php">Manage Data</a></li>
+                <li><a href="user_edit.php">Setting</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        
+        </div>
+    </div>
+    <div class="main_content">
+        <div class="header">Welcome to Perwaani Record Management System</div>
+
+    </div>
+    <div class="info">
+
+        
+    </div>
+
+    
+</body>
+</html>
